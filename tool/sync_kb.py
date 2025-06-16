84e603c62f8ea4691ba26bec31d70ba72d1193fb
yunji cho <132728613+YUMZII@users.noreply.github.com>
2025-06-16 14:50:31 +0900

    Update sync_kb.py

--- tool/sync_kb.py
+++ tool/sync_kb.py
@@ -5,5 +5,5 @@ def sync_kb():
     kb_client = boto3.client("bedrock-agent", region_name=AWS_REGION)
     response = kb_client.start_ingestion_job(
         knowledgeBaseId=BEDROCK_KB_ID,
-        dataSourceId="JMG9NFQW5H",  # 연결된 Data Source ID
+        dataSourceId=" ",  # 연결된 Data Source ID
     )
